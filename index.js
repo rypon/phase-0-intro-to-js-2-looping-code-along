c779603a78caa0634ae5d403db2ff250e6228856
Ryan O'Neill <ryanponeill00@gmail.com>
2021-12-31 12:22:32 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solutions in this file
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//     for (let i = 0; i < gifts.length; i++) {
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//         debugger;
+//     }
+//     return gifts;
+// }
+
+// wrapGifts(gifts);
+
+
+const names = [];
+
+function writeCards(names) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+        debugger;
+    }
+    return messages;
+}
+
+function countDown(posNum) {
+    while (posNum >= 0) {
+        console.log(posNum);
+        posNum--;
+    }
+}
